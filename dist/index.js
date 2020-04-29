c2594e7ef8490adf8b280e105c890d4ce1147720
Jeff King <jeffking@gmail.com>
2020-04-29 13:52:01 -0700

    Reduce.

--- dist/index.js
+++ dist/index.js
@@ -7,17 +7,21 @@ async function run() {
 	try {
 		const github = new GitHub(process.env.GITHUB_TOKEN)
 		const {owner, repo} = context.repo;
-		const release_id = core.getInput('asset_name', {required: true})
+		// const release_id = core.getInput('asset_name', {required: true})
 
-		const {data: assets} = await github.repos.listAssetsForRelease({
-			owner, repo, release_id,
+		// const {data: assets} = await github.repos.listAssetsForRelease({
+		// 	owner, repo, release_id,
+		// })
+
+		// for (const asset of assets) {
+		// 	await github.repos.deleteReleaseAsset({
+		// 		owner, repo, asset_id: asset.id
+		// 	})
+		// }
+
+		await github.repos.deleteReleaseAsset({
+			owner, repo, asset_id: 20256621
 		})
-
-		for (const asset of assets) {
-			await github.repos.deleteReleaseAsset({
-				owner, repo, asset_id: asset.id
-			})
-		}
 	} catch (error) {
 		core.setFailed(error.message)
 	}
