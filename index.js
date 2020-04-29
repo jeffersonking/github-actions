a715690867b09f9c17182e6af2b3c5d113a31a0f
Jeff King <jeffking@gmail.com>
2020-04-29 12:56:51 -0700

    Add action: Delete all Release Assets

---
+++ index.js
@@ -0,0 +1,20 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const {GitHub} = require('@actions/github')
+
+try {
+	const github = new GitHub(process.env.GITHUB_TOKEN)
+
+	const {data: assets} = await github.repos.listAssetsForRelease({ release_id: 25992490 })
+	for (const asset of assets) {
+		console.log(`Asset ${asset.id}`)
+	}
+
+	// const r = await github.repos.deleteReleaseAsset({ asset_id: 20256621 })
+
+} catch (error) {
+	core.setFailed(error.message)
+}
+
+// repos/jeffersonking/github-actions/releases
+// repos/jeffersonking/github-actions/releases/25992490/assets
