a3401db2941cf52bedfd6865f05ef683b3ddb45a
Jeff King <jeffking@gmail.com>
2020-04-29 13:33:03 -0700

    Refactor.

--- dist/index.js
+++ dist/index.js
@@ -1,31 +1,26 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
 const {GitHub} = require('@actions/github')
 
+// repos/jeffersonking/github-actions/releases
+// repos/jeffersonking/github-actions/releases/25992490/assets
 async function run() {
 	try {
-		console.log(00)
 		const github = new GitHub(process.env.GITHUB_TOKEN)
+		const {owner, repo} = context.repo;
 
-		console.log(11)
 		const {data: assets} = await github.repos.listAssetsForRelease({
-			owner: 'jeffersonking',
-			repo: 'github-actions',
+			owner, repo,
 			release_id: 25992490,
 		})
-		console.log(22)
+
 		for (const asset of assets) {
 			console.log(`Asset ${asset.id}`)
 		}
 
 		// const r = await github.repos.deleteReleaseAsset({ asset_id: 20256621 })
-
 	} catch (error) {
 		core.setFailed(error.message)
 	}
 }
 
 run()
-
-// repos/jeffersonking/github-actions/releases
-// repos/jeffersonking/github-actions/releases/25992490/assets
