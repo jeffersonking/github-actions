6dc18a3b9952ba441721e01342c860a76c1df1cb
Jeff King <jeffking@gmail.com>
2020-04-29 14:05:52 -0700

    Fix.

--- dist/index.js
+++ dist/index.js
@@ -7,23 +7,18 @@ const {context, GitHub} = require('@actions/github')
 async function run() {
 	try {
 		const github = new GitHub(process.env.GITHUB_TOKEN)
-		const {owner, repo} = context.repo;
-		const release_id = core.getInput('asset_name', {required: true})
+		const { owner, repo } = context.repo;
+		const release_id = core.getInput('release_id', { required: true })
 
 		const {data: assets} = await github.repos.listAssetsForRelease({
 			owner, repo, release_id,
 		})
 
 		for (const asset of assets) {
-			console.log(owner, repo, asset.id)
-			// await github.repos.deleteReleaseAsset({
-			// 	owner, repo, asset_id: asset.id
-			// })
+			await github.repos.deleteReleaseAsset({
+				owner, repo, asset_id: asset.id
+			})
 		}
-
-		// await github.repos.deleteReleaseAsset({
-		// 	owner, repo, asset_id: 20256621
-		// })
 	} catch (error) {
 		core.setFailed(error.message)
 	}
