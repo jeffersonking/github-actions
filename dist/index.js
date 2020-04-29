0c48371a14bf2843333945b8de4a947d01bcf40c
Jeff King <jeffking@gmail.com>
2020-04-29 13:53:51 -0700

    Restore.

--- dist/index.js
+++ dist/index.js
@@ -9,19 +9,19 @@ async function run() {
 		const {owner, repo} = context.repo;
 		// const release_id = core.getInput('asset_name', {required: true})
 
-		// const {data: assets} = await github.repos.listAssetsForRelease({
-		// 	owner, repo, release_id,
-		// })
-
-		// for (const asset of assets) {
-		// 	await github.repos.deleteReleaseAsset({
-		// 		owner, repo, asset_id: asset.id
-		// 	})
-		// }
-
-		await github.repos.deleteReleaseAsset({
-			owner, repo, asset_id: 20256621
+		const {data: assets} = await github.repos.listAssetsForRelease({
+			owner, repo, release_id,
 		})
+
+		for (const asset of assets) {
+			await github.repos.deleteReleaseAsset({
+				owner, repo, asset_id: asset.id
+			})
+		}
+
+		// await github.repos.deleteReleaseAsset({
+		// 	owner, repo, asset_id: 20256621
+		// })
 	} catch (error) {
 		core.setFailed(error.message)
 	}
