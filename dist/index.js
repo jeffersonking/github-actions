b84c87d9d859b31d132dfb54a0f0372a96ceb267
Jeff King <jeffking@gmail.com>
2020-04-29 13:34:45 -0700

    Actually delete.

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core')
-const {GitHub} = require('@actions/github')
+const {context, GitHub} = require('@actions/github')
 
 // repos/jeffersonking/github-actions/releases
 // repos/jeffersonking/github-actions/releases/25992490/assets
@@ -9,15 +9,12 @@ async function run() {
 		const {owner, repo} = context.repo;
 
 		const {data: assets} = await github.repos.listAssetsForRelease({
-			owner, repo,
-			release_id: 25992490,
+			owner, repo, release_id: 25992490,
 		})
 
 		for (const asset of assets) {
-			console.log(`Asset ${asset.id}`)
+			await github.repos.deleteReleaseAsset({ asset_id: asset.id })
 		}
-
-		// const r = await github.repos.deleteReleaseAsset({ asset_id: 20256621 })
 	} catch (error) {
 		core.setFailed(error.message)
 	}
