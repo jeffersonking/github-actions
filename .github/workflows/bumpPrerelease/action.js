ccdfb91da53df9f8bd892d0cf04273ca14f915d5
Jeff King <jeffking@gmail.com>
2020-09-10 13:45:54 -0700

    Update to match.

--- .github/workflows/bumpPrerelease/action.js
+++ .github/workflows/bumpPrerelease/action.js
@@ -1,24 +1,34 @@
-const { readFileSync, writeFileSync } = require('fs')
-const { execFileSync } = require('child_process')
-const { parse } = require('semver')
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
-const package = JSON.parse(readFileSync('./package.json', 'utf8'))
-const packageVer = parse(package.version)
+// Run `ncc build action.js --out .` to produce `index.js`
+const { execFileSync } = require('child_process');
+const { readFileSync, writeFileSync } = require('fs');
+const { parse } = require('semver');
 
-let prerelease = 0 // Main was changed, or no prev version, restart prerelease from 0.
+const package = JSON.parse(readFileSync('../package.json', 'utf8'));
+const packageVer = parse(package.version);
+
+let prerelease = 0; // Main was changed, or no prev version, restart prerelease from 0.
 try {
+	console.log('-----');
+	execFileSync('git', ['log', '--oneline']);
+	console.log('-----');
+	execFileSync('git', ['fetch', '--depth=10']);
+	console.log('-----');
+	execFileSync('git', ['fetch', '--tags']);
+	console.log('-----');
 	// `abbrev=0` finds the closest tagname without any suffix.
-	execFileSync('git', ['fetch', '--depth=10'])
-	execFileSync('git', ['fetch', '--tags'])
-	const tag = execFileSync('git', ['describe', '--tags', '--abbrev=0'], { encoding: 'utf8' }).trim()
-	const lastReleaseVer = parse(tag)
+	// HEAD~1 assuming the latest commit hasn't been tagged by this Action yet.
+	const tag = execFileSync('git', ['describe', '--tags', '--abbrev=0', 'HEAD~1'], { encoding: 'utf8' }).trim();
+	const lastReleaseVer = parse(tag);
 	if (packageVer.compareMain(lastReleaseVer) === 0) {
-		prerelease = lastReleaseVer.prerelease[0] + 1 // Main is equal, auto-increment the prerelease.
+		prerelease = lastReleaseVer.prerelease[0] + 1; // Main is equal, auto-increment the prerelease.
 	}
 } catch (error) {
 }
 
-packageVer.prerelease = [ prerelease ]
-package.version = packageVer.format()
-console.log('Computed package version:', package.version)
-writeFileSync('./package.json', JSON.stringify(package, null, 4))
+packageVer.prerelease = [ prerelease ];
+package.version = packageVer.format();
+console.log('Computed package version:', package.version);
+writeFileSync('../package.json', JSON.stringify(package, null, 4));
