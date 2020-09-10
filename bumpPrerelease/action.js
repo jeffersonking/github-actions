e29cc49f0e46e67b85b3d51a96c8e3f3d336013f
Jeff King <jeffking@gmail.com>
2020-09-10 02:33:33 -0700

    Gracefully handle no tags.

--- bumpPrerelease/action.js
+++ bumpPrerelease/action.js
@@ -1,24 +1,24 @@
-const core = require('@actions/core')
 const { readFileSync, writeFileSync } = require('fs')
 const { execFileSync } = require('child_process')
 const { parse } = require('semver')
 
-try {
-	const package = JSON.parse(readFileSync('./package.json', 'utf8'))
-	const packageVer = parse(package.version)
+const package = JSON.parse(readFileSync('./package.json', 'utf8'))
+const packageVer = parse(package.version)
 
+let prerelease = 0 // Main was changed, or no prev version, restart prerelease from 0.
+try {
 	// `abbrev=0` finds the closest tagname without any suffix.
+	execFileSync('git', ['fetch', '--depth=10'])
+	execFileSync('git', ['fetch', '--tags'])
 	const tag = execFileSync('git', ['describe', '--tags', '--abbrev=0'], { encoding: 'utf8' }).trim()
 	const lastReleaseVer = parse(tag)
-	
-	const prerelease = packageVer.compareMain(lastReleaseVer) === 0
-		? lastReleaseVer.prerelease[0] + 1	// Main is equal, auto-increment the prerelease.
-		: 0 								// Main was changed, restart prerelease from 0.
-	
-	packageVer.prerelease = [ prerelease ]
-	package.version = packageVer.format()
-	console.log('Computed package version:', package.version)
-	writeFileSync('./package.json', JSON.stringify(package, null, 4))
+	if (packageVer.compareMain(lastReleaseVer) === 0) {
+		prerelease = lastReleaseVer.prerelease[0] + 1 // Main is equal, auto-increment the prerelease.
+	}
 } catch (error) {
-	core.setFailed(error.message)
 }
+
+packageVer.prerelease = [ prerelease ]
+package.version = packageVer.format()
+console.log('Computed package version:', package.version)
+writeFileSync('./package.json', JSON.stringify(package, null, 4))
