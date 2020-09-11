ae92695ad98680499b16dac4bb700c9d5c86ed39
Jeff King <jeffking@gmail.com>
2020-09-11 13:46:36 -0700

    36

--- .github/workflows/bumpPrerelease/action.js
+++ .github/workflows/bumpPrerelease/action.js
@@ -12,12 +12,12 @@ const packageVer = parse(package.version);
 
 let prerelease = 0; // Main was changed, or no prev version, restart prerelease from 0.
 try {
-	core.startGroup('Fetching tags')
+	core.startGroup('Fetching tags');
 	execFileSync('git', ['fetch', '--tags']);
-	core.endGroup()
+	core.endGroup();
 
-	core.startGroup('Looking for tags from commit history')
-	core.info(execFileSync('git', ['log', '--oneline'], { encoding: 'utf8' }));
+	core.startGroup('Looking for tags from commit history');
+	console.log(execFileSync('git', ['log', '--oneline'], { encoding: 'utf8' }));
 	// `abbrev=0` finds the closest tagname without any suffix.
 	// HEAD~1 assuming the latest commit hasn't been tagged by this Action yet.
 	const tag = execFileSync('git', ['describe', '--tags', '--abbrev=0', 'HEAD~1'], { encoding: 'utf8' }).trim();
@@ -25,7 +25,7 @@ try {
 	if (packageVer.compareMain(lastReleaseVer) === 0) {
 		prerelease = lastReleaseVer.prerelease[0] + 1; // Main is equal, auto-increment the prerelease.
 	}
-	core.endGroup()
+	core.endGroup();
 } catch (error) {
 }
 
